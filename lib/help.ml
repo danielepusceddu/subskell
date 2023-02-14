68d116e0ff5e87e833a0f9b601ce2976ff1fcfb8
Daniele Pusceddu <43929743+danielepusceddu@users.noreply.github.com>
2023-02-14 12:43:59 +0100

    Rename MapString to StringMap for consistency

--- lib/help.ml
+++ lib/help.ml
@@ -1,13 +1,13 @@
-module MapString = Map.Make(String);;
+module StringMap = Map.Make(String);;
 module IntMap = Map.Make(Int);;
 module IntSet = Set.Make(Int);;
 
 (* function to check for duplicates in a list of strings using a Map *)
 let has_dups lst =
   (* create a map from the list with each element as the key and a unit value *)
-  let map = List.fold_left (fun m s -> MapString.add s () m)MapString. empty lst in
+  let map = List.fold_left (fun m s -> StringMap.add s () m)StringMap. empty lst in
   (* return true if the size of the map is less than the length of the list *)
-  List.length lst > MapString.cardinal map
+  List.length lst > StringMap.cardinal map
 
 let find_max l = List.fold_left 
   (fun x y -> if x > y then x else y)
