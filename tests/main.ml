c0574157842682b1d4552f93c807eb0bbf83ac38
Federico <96267363+dalps@users.noreply.github.com>
2023-02-15 18:48:52 +0100

    Refactor eval and typecheck tests

--- tests/main.ml
+++ tests/main.ml
@@ -4,37 +4,47 @@ open SubskellLib.Typesstatic;;
 open SubskellLib.Typesruntime;;
 open SubskellLib.Typecheck;;
 
-print_endline("\nTesting powers of 2...");
-assert ("
-let power : int -> int -> int = 
-  fun n -> fun m -> if m = 0 then 1 else n*power n (m-1) 
-in let powersof2 = power 2 in
-powersof2 10"  |> parse |> (eval_prog (-1)) = Ok (ET (CNum 1024)));
-print_endline("Test passed.");
+(* Format: (test label, source code, eval result) *)
+let eval_tests = [
+  ("powers of 2",
+   "let power : int -> int -> int = 
+    fun n -> fun m -> if m = 0 then 1 else n*power n (m-1) 
+    in let powersof2 = power 2 in
+    powersof2 10", Ok (ET (CNum 1024)));
+  ("Fibonacci",
+   "let fib : int -> int = fun n -> 
+    if n = 0 then 0 
+    else if n = 1 then 1 
+    else fib (n-1) + fib (n-2)
+    in fib 10", Ok (ET (CNum 55)));
+  ("id",
+   "let id = fun x -> x
+    in if id true then id 5 else id 9", Ok (ET (CNum 5)));
+  ("eq polymorphism", 
+   "let id = fun x -> x
+    in let eq = (=)
+    in if (id true) = true && (id 5) = 5 
+      then id 1337 
+      else id 420", Ok (ET (CNum 1337)));
+]
+;;
 
-print_endline("\nTesting Fibonacci...");
-assert ("
-let fib : int -> int = fun n -> 
-  if n = 0 then 0 
-  else if n = 1 then 1 
-  else fib (n-1) + fib (n-2)
-in fib 10" |> parse |> (eval_prog (-1)) = Ok (ET (CNum 55)));
-print_endline("Test passed.");
-
-print_endline("\nTesting id...");
-assert ("
-let id = fun x -> x
-in if id true then id 5 else id 9" |> parse |> (eval_prog (-1)) = Ok (ET (CNum 5)));
-print_endline("Test passed.");
-
-print_endline("\nTesting eq polymorphism...");
-assert ("
-let id = fun x -> x
-in let eq = (=)
-in if (id true) = true && (id 5) = 5 
-   then id 1337 
-   else id 420" |> parse |> (eval_prog (-1)) = Ok (ET (CNum 1337)));
-print_endline("Test passed.");
+(* Format: (test label, source code, main type, list of names and their expected types) *)
+let typecheck_tests = [
+  ("powers of 2", 
+   "let power = 
+    fun n -> fun m -> if m = 0 then 1 else n*power n (m-1) 
+    in let powersof2 = power 2 in
+    powersof2 10", TInt, [("power", ([],TFun(TInt, TFun(TInt, TInt)))); ("powersof2", ([],TFun(TInt, TInt)))]);
+  ("id",
+   "let id = fun x -> x
+    in if id true then id 5 else id 9", TInt, [("id", ([1],TFun(TVar 1, TVar 1)))]);
+  ("eq",
+   "let booleq = (=) true in
+    let inteq = (=) 1
+    in (=)", TFun(TVar 6, TFun (TVar 6, TBool)), [("booleq", ([],TFun(TBool, TBool))); ("inteq", ([],TFun(TInt, TBool)))])
+]
+;;
 
 let rec find_1st_letin (e: aexpr) (x: ide) = match e with
    | ALetIn(y,_,_,_) as needle when x=y -> Some needle
@@ -57,46 +67,50 @@ let rec find_1st_letin (e: aexpr) (x: ide) = match e with
   | AName _
   | ANum _
   | ABool _ -> None
-  in
+;;
+
 let has_type e (x: ide) (t: tscheme) = match find_1st_letin e x with
    | Some ALetIn(_,t2,_,_) -> t=t2
    | _ -> failwith ("test has no letin with " ^ x)
-in
+;;
 
-print_endline("\nTesting powers of 2 typecheck...");
-assert (match ("
-let power = 
-  fun n -> fun m -> if m = 0 then 1 else n*power n (m-1) 
-in let powersof2 = power 2 in
-powersof2 10" |> parse |> (tinfer_expr static_tenv)) with
-| Ok(maint, e') -> let has_type = has_type e' in 
-maint=TInt && (has_type "power" ([],TFun(TInt, TFun(TInt, TInt)))) && (has_type "powersof2" ([],TFun(TInt, TInt)))
-| _ -> false
-);
-print_endline("Test passed.");
 
-print_endline("\nTesting id typecheck...");
-assert (match ("
-let id = fun x -> x
-in if id true then id 5 else id 9" |> parse |> (tinfer_expr static_tenv)) with
-| Ok(maint, e') -> let has_type = has_type e' in 
-maint=TInt && (has_type "id" ([1],TFun(TVar 1, TVar 1)))
-| _ -> false
-);
-print_endline("Test passed.");
+let%test _ =
+  print_newline();
+  print_endline ("*** Testing eval...");
+  List.fold_left
+  (fun b (label,source,evalres) ->
+    print_string ("\n* Testing " ^ label ^ "...");
+    let past = parse source
+    in let b' = (eval_prog (-1) past) = evalres 
+    in print_string (" " ^ (if b' then "[OK]" else "[NO : expected " ^ string_of_evalresult evalres ^ "]"));
+    b && b')
+  true
+  eval_tests
 
-print_endline("\nTesting eq typecheck...");
-assert (match ("
-let booleq = (=) true in
-let inteq = (=) 1
-in (=)" |> parse |> (tinfer_expr static_tenv)) with
-| Ok(maint, e') -> let has_type = has_type e' in 
-maint=(TFun(TVar 6, TFun (TVar 6, TBool))) && (has_type "booleq" ([],TFun(TBool, TBool)))  && (has_type "inteq" ([],TFun(TInt, TBool)))
-| _ -> false
-);
-print_endline("Test passed.");
 
-print_endline("\nTesting stricter type success...");
+let%test _ =
+  print_endline("\n\n\n*** Testing typecheck...");
+  List.fold_left
+  (fun b (label,source,maintype,subtests) ->
+    let past = parse source in
+    print_string ("\n* Testing " ^ label ^ "...");
+    let b' = (match (tinfer_expr static_tenv past) with
+      | Ok(maint, e') -> (let has_type' = has_type e' 
+        in (maint = maintype) && (List.fold_left
+          (fun b (id,tsch) -> b && (has_type' id tsch))
+          true
+          subtests))
+      | _ -> false)
+
+    in print_string (" " ^ (if b' then "[OK]" else "[NO : expected " ^ string_of_type maintype ^ "]"));
+    b && b'
+  )
+  true
+  typecheck_tests;;
+
+
+print_endline("\n\nTesting stricter type success...");
 assert (match ("
 let inteq : int -> int -> bool = (=)
 in let booleq : bool -> bool -> bool = (=)
